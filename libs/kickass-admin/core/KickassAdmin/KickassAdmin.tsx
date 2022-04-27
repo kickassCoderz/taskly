bfc52d713862d7c429421d98492cdb62a4a5721e
Saša Fazlić <45801671+Fazla-GroM@users.noreply.github.com>
2022-04-27 10:05:34 +0200

    feat: added absolute path support and type alias for kickass-admin (#8)

--- libs/kickass-admin/core/KickassAdmin/KickassAdmin.tsx
+++ libs/kickass-admin/core/KickassAdmin/KickassAdmin.tsx
@@ -1,17 +1,27 @@
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { useState } from 'react'
+import { QueryClient, QueryClientConfig, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { AuthServiceProvider, DataServiceProvider, RealtimeServiceProvider } from '../../contexts'
 import { IAuthService, IDataService, IRealtimeService } from '../types'
 export interface IKickassAdminProps {
     children: React.ReactNode
+    queryClientInstance?: QueryClient
+    queryClientConfig?: QueryClientConfig
     dataService?: IDataService
     authService?: IAuthService
     realtimeService?: IRealtimeService
 }
 
-const KickassAdmin = ({ children, dataService, authService, realtimeService }: IKickassAdminProps) => {
-    const queryClient = new QueryClient()
+const KickassAdmin = ({
+    children,
+    dataService,
+    authService,
+    realtimeService,
+    queryClientInstance,
+    queryClientConfig
+}: IKickassAdminProps) => {
+    const [queryClient] = useState(() => queryClientInstance || new QueryClient(queryClientConfig))
 
     return (
         <QueryClientProvider client={queryClient}>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,19 @@
+import { KickassAdmin } from '@kickass-admin'
 import { NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
 import { ThemeProvider as NextThemeProvider } from 'next-themes'
-import { useState } from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { themeProviderValues } from '../theme'
 
 const App = ({ Component, pageProps }: AppProps) => {
-    const [queryClient] = useState(() => new QueryClient())
-
     return (
-        <NextThemeProvider defaultTheme="system" attribute="class" value={themeProviderValues}>
-            <NextUIProvider>
-                <QueryClientProvider client={queryClient}>
+        <KickassAdmin>
+            <NextThemeProvider defaultTheme="system" attribute="class" value={themeProviderValues}>
+                <NextUIProvider>
                     <Component {...pageProps} />
-                    <ReactQueryDevtools />
-                </QueryClientProvider>
-            </NextUIProvider>
-        </NextThemeProvider>
+                </NextUIProvider>
+            </NextThemeProvider>
+        </KickassAdmin>
     )
 }
 
