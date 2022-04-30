5e0f0d5ec4169a6088a7298bacbebaf87d076b53
Sasa Fazlic <fazlic.sasa@gmail.com>
2022-05-01 00:58:20 +0200

    feat(tasks): added layout

--- pages/tasks/index.tsx
+++ pages/tasks/index.tsx
@@ -1,12 +1,26 @@
-import { useGetList } from '@kickass-admin'
-import { TTask } from 'types'
+import { Row, Text } from '@nextui-org/react'
+import { AppLayout, AppPageAppBar, AppPageContainer } from 'components'
+import Link from 'next/link'
 
 const TasksPage = () => {
-    const listQuery = useGetList<TTask[], unknown>({
-        resource: 'tasks'
-    })
+    return (
+        <>
+            <AppPageAppBar>
+                <Row>
+                    <Link passHref href="/dashboard">
+                        <Text h4>Tasks</Text>
+                    </Link>
+                </Row>
+            </AppPageAppBar>
+            <AppPageContainer>
+                <Text>Hello from tasks page</Text>
+            </AppPageContainer>
+        </>
+    )
+}
 
-    return <div>Tasks</div>
+TasksPage.getLayout = (page: React.ReactElement) => {
+    return <AppLayout>{page}</AppLayout>
 }
 
 export default TasksPage
