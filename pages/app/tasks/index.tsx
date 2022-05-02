aac7b8ecd5dbc743f2791e68ed8cccd3e9d419bf
capJavert <ante@kickass.website>
2022-05-02 18:37:01 +0200

    feat: list tasks

--- pages/app/tasks/index.tsx
+++ pages/app/tasks/index.tsx
@@ -1,9 +1,21 @@
-import { Grid, Row, Table, Text } from '@nextui-org/react'
+import { useGetList } from '@kickass-admin'
+import { Grid, Link as NextUILink, Row, Table, Text } from '@nextui-org/react'
 import { AppLayout, AppPageAppBar, AppPageContainer } from 'components'
 import Link from 'next/link'
 import { NextSeo } from 'next-seo'
+import { TTask } from 'types'
 
 const AppTasksPage = () => {
+    const { data: tasks, isLoading } = useGetList<TTask[], Error>({
+        resource: 'tasks',
+        params: {
+            pagination: {
+                page: 1,
+                perPage: 100
+            }
+        }
+    })
+
     return (
         <>
             <NextSeo title="My Tasks" />
@@ -15,44 +27,40 @@ const AppTasksPage = () => {
                 </Row>
             </AppPageAppBar>
             <AppPageContainer>
-                <Grid.Container gap={2}>
-                    <Grid xs={12}>
-                        <Table
-                            containerCss={{
-                                width: '100%'
-                            }}
-                            aria-label="Example table with static content"
-                        >
-                            <Table.Header>
-                                <Table.Column>NAME</Table.Column>
-                                <Table.Column>ROLE</Table.Column>
-                                <Table.Column>STATUS</Table.Column>
-                            </Table.Header>
-                            <Table.Body>
-                                <Table.Row key="1">
-                                    <Table.Cell>Tony Reichert</Table.Cell>
-                                    <Table.Cell>CEO</Table.Cell>
-                                    <Table.Cell>Active</Table.Cell>
+                <Grid xs={12}>
+                    <Table
+                        aria-label="My Tasks"
+                        css={{
+                            height: 'auto',
+                            minWidth: '100%'
+                        }}
+                    >
+                        <Table.Header>
+                            <Table.Column>ID</Table.Column>
+                            <Table.Column>TITLE</Table.Column>
+                            <Table.Column>CONTENT</Table.Column>
+                            <Table.Column>LINK</Table.Column>
+                        </Table.Header>
+                        <Table.Body loadingState={!tasks && isLoading}>
+                            {tasks?.map(task => (
+                                <Table.Row key={task.id}>
+                                    <Table.Cell>{task.id}</Table.Cell>
+                                    <Table.Cell>{task.title}</Table.Cell>
+                                    <Table.Cell>{task.content?.substring(0, 32)}</Table.Cell>
+                                    {!!task.link && (
+                                        <Table.Cell>
+                                            <Link passHref href={task.link}>
+                                                <NextUILink target="_blank" title="Open original issue" underline>{`${
+                                                    task.provider || 'original'
+                                                } issue`}</NextUILink>
+                                            </Link>
+                                        </Table.Cell>
+                                    )}
                                 </Table.Row>
-                                <Table.Row key="2">
-                                    <Table.Cell>Zoey Lang</Table.Cell>
-                                    <Table.Cell>Technical Lead</Table.Cell>
-                                    <Table.Cell>Paused</Table.Cell>
-                                </Table.Row>
-                                <Table.Row key="3">
-                                    <Table.Cell>Jane Fisher</Table.Cell>
-                                    <Table.Cell>Senior Developer</Table.Cell>
-                                    <Table.Cell>Active</Table.Cell>
-                                </Table.Row>
-                                <Table.Row key="4">
-                                    <Table.Cell>William Howard</Table.Cell>
-                                    <Table.Cell>Community Manager</Table.Cell>
-                                    <Table.Cell>Vacation</Table.Cell>
-                                </Table.Row>
-                            </Table.Body>
-                        </Table>
-                    </Grid>
-                </Grid.Container>
+                            ))}
+                        </Table.Body>
+                    </Table>
+                </Grid>
             </AppPageContainer>
         </>
     )
--- types/models/tasks/index.ts
+++ types/models/tasks/index.ts
@@ -1,3 +1,7 @@
 export type TTask = {
     id: string
+    title: string
+    content: string
+    link: string
+    provider: string
 }
