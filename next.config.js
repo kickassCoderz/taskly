9a173568eadcb72e82a12c36be09234678fecf12
capJavert <ante@kickass.website>
2022-05-09 12:31:02 +0200

    fea: remove unallowed imports from node_modules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const removeImports = require('next-remove-imports')()
+
 const nextConfig = {
     reactStrictMode: true
 }
 
-module.exports = nextConfig
+module.exports = removeImports(nextConfig)
